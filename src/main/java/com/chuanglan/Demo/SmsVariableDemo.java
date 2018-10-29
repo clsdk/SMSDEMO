c7e5a60bd17cb94e5b5bad0ec6fa7a75cb20bbb4
clsdk <44493364+clsdk@users.noreply.github.com>
2018-10-29 14:20:50 +0800

    Update SmsVariableDemo.java

--- src/main/java/com/chuanglan/Demo/SmsVariableDemo.java
+++ src/main/java/com/chuanglan/Demo/SmsVariableDemo.java
@@ -9,15 +9,15 @@ import java.util.Map;
 public class SmsVariableDemo {
     public static void main(String[] args) {
         Map map = new HashMap();
-        map.put("account","N4310605");
-        map.put("password","yQ8heC4jsAacc3");
-        map.put("msg","【253云通讯】您的工单编号{$var},已完成处理");
-        map.put("params","15317652370,112;");
+        map.put("account","N*******");
+        map.put("password","**********");
+        map.put("msg","【253云通讯】尊敬的{$var},您的包裹编号{$var},已送达");
+        map.put("params","153********,张先生,112;");
         map.put("report","true");
         map.put("sendtime","");
         map.put("extend","");
         map.put("uid","");
         JSONObject js = (JSONObject) JSONObject.toJSON(map);
-        System.out.println(SendByPost.sendSmsByPost("https://smssh1.253.com/msg/variable/json",js.toString()));
+        System.out.println(SendByPost.sendSmsByPost("https://xxx/msg/variable/json",js.toString()));
     }
 }

9d74fb56d91034d35cb469e8b5f8705c81891268
clsdk <44493364+clsdk@users.noreply.github.com>
2018-10-29 14:17:57 +0800

    Update SmsMoDemo.java

--- src/main/java/com/chuanglan/Demo/SmsMoDemo.java
+++ src/main/java/com/chuanglan/Demo/SmsMoDemo.java
@@ -9,10 +9,10 @@ import java.util.Map;
 public class SmsMoDemo {
     public static void main(String[] args) {
         Map map = new HashMap();
-        map.put("account","N4310605");
-        map.put("password","yQ8heC4jsAacc3");
+        map.put("account","N*******");
+        map.put("password","**********");
         map.put("count",20);
         JSONObject js = (JSONObject) JSONObject.toJSON(map);
-        System.out.println(SendByPost.sendSmsByPost("https://smssh1.253.com/msg/pull/mo",js.toString()));
+        System.out.println(SendByPost.sendSmsByPost("https://xxx/msg/pull/mo",js.toString()));
     }
 }

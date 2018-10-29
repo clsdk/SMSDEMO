d484d643f0e5627e2515636643f3527d6c34a19a
clsdk <44493364+clsdk@users.noreply.github.com>
2018-10-29 14:15:34 +0800

    Update SmsSendDemo.java

--- src/main/java/com/chuanglan/Demo/SmsSendDemo.java
+++ src/main/java/com/chuanglan/Demo/SmsSendDemo.java
@@ -9,15 +9,15 @@ import java.util.Map;
 public class SmsSendDemo {
     public static void main(String[] args) {
         Map map = new HashMap();
-        map.put("account","N4310605");
-        map.put("password","yQ8heC4jsAacc3");
+        map.put("account","N******");
+        map.put("password","**********");
         map.put("msg","【253云通讯】您的验证码是123456");
-        map.put("phone","15317652370");
+        map.put("phone","153********");
         map.put("report","true");
         map.put("sendtime","");
         map.put("extend","");
         map.put("uid","");
         JSONObject js = (JSONObject) JSONObject.toJSON(map);
-        System.out.println(SendByPost.sendSmsByPost("https://smssh1.253.com/msg/send/json",js.toString()));
+        System.out.println(SendByPost.sendSmsByPost("https://xxx/msg/send/json",js.toString()));
     }
 }
